1d5baec15dd10d5246e0b18116939775d1237627
charles <chong039@ucr.edu>
2025-09-12 20:31:50 -0700

    remove all

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Puzzle from '$lib/Puzzle.svelte';
-</script>
-
-<h1>Welcome to Crosswords</h1>
-
-<Puzzle/>
--- src/routes/crossword/[game_id]/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    import { page } from '$app/state';
-</script>
-
-<h1> this is a game {page.url.pathname}</h1>
