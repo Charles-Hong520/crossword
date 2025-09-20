1dd9c46283db73c2efeb1aa1922928064598e238
charles <chong039@ucr.edu>
2025-09-19 22:54:07 -0700

    display clues

--- src/components/Clues.jsx
+++ src/components/Clues.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react';
-function Clues({puzzle, setHighlighted}) {
+import '@/styles/Clues.css';
+function Clues({ puzzle, setHighlighted }) {
   const across = puzzle.across;
   const down = puzzle.down;
+
+
+
   return (
     <>
-      <div>
-        {/* {down[1].clue} */}
+      <div className='container'>
+        <ul>
+          {Object.entries(across).map(([number, data]) => (
+            <li key={number}>
+              {number}. {data.clue}
+            </li>
+          ))}
+        </ul>
+        <ul >
+          {Object.entries(down).map(([number, data]) => (
+            <li key={number}>
+              {number}. {data.clue}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -14,8 +14,7 @@ function Puzzle() {
       6: { clue: "Still in it", start: [1, 0] },
       7: { clue: "Zero-shaped food that's a slang for zero, in tennis", start: [2, 0] },
       8: { clue: "Gooey fireside treat", start: [3, 0] },
-      9: { clue: "Civil wrongs, in law", start: [4, 0] },
-      owner: [1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9]
+      9: { clue: "Civil wrongs, in law", start: [4, 0] }
     },
     down: {
       1: { clue: "___ Blue Ribbon", start: [0, 0] },
@@ -23,20 +22,28 @@ function Puzzle() {
       3: { clue: "Vitality", start: [0, 2] },
       4: { clue: "Tennis legend Chris", start: [0, 3] },
       5: { clue: "Tennis legend Monica", start: [0, 4] },
-      owner: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
-    answer: "pavesalivebagelsmoretorts"
+    answer: "pavesalivebagelsmoretorts",
+    owner: {
+      across: [1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9],
+      down: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    }
   };
-
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "row"
+  }
   return (
     <>
-      <Grid
-        puzzle={puzzle}
-      />
-      <Clues
-        puzzle={puzzle}
-        setHighlighted={setHighlighted}
-      />
+      <div style={containerStyle}>
+        <Grid
+          puzzle={puzzle}
+        />
+        <Clues
+          puzzle={puzzle}
+          setHighlighted={setHighlighted}
+        />
+      </div>
     </>
   )
 }
