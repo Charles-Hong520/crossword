4284e55327a0f2415630cd61c6d0fd0bc29616bc
charles <chong039@ucr.edu>
2025-09-26 16:55:56 -0700

    change name

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/crossword/",
   resolve: {
     alias: {
       // Define your aliases here
@@ -13,5 +14,4 @@ export default defineConfig({
       // 'components': resolve(__dirname, 'src/components'),
     },
   },
-  base:"/crossword"
 });
